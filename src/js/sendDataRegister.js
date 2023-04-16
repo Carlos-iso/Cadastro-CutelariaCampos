c0a6cabdd336db942edcb17f8f5e3ed26a5f77db
Carlos-iso <carlosbalak2002@gmail.com>
2023-04-16 14:13:56 -0300

    Message bug

--- src/js/sendDataRegister.js
+++ src/js/sendDataRegister.js
@@ -5,7 +5,7 @@ const urlLocal = "http://localhost:3000";
 const urlRender = "https://api-cutelariacampos.onrender.com";
 const urlBase = urlRender;
 let clearFileld = "Campos Vazios";
-let $msg;
+let $msg = "Erro";
 
 async function validForm() {
   if (
@@ -33,35 +33,42 @@ async function validForm() {
     })
       .then(awtResponce)
       .then(dataPost)
-      .then(msgSuccess)
-      .then(resetFilds)
+      .then(msgSuccessStyle)
+      .then(msgSuccessText)
       .catch(() => msgError());
   } else {
-    $msg = $msgerror.innerHTML = `<strong>${clearFileld}</strong>`;
-    msgError()
+    $msg = clearFileld;
+    msgErrorStyle();
+    msgErrorText();
   }
 }
 
 function awtResponce(response) {
-  return response.text();
+  return response.json();
 }
 
 function dataPost(dataPost) {
-  $msg = dataPost
+  $msg = Object.values(dataPost);
 }
 
-function msgSuccess() {
-  $msgsuccess.setAttribute("style", "display: block");
+function msgSuccessText() {
   $msgsuccess.innerHTML = `<strong>${$msg}</strong>`;
+}
+
+function msgSuccessStyle() {
+  $msgsuccess.setAttribute("style", "display: block");
   $msgerror.setAttribute("style", "display: none");
   $msgerror.innerHTML = "";
 }
 
-function msgError() {
+function msgErrorText() {
+  $msgerror.innerHTML = `<strong>${$msg}</strong>`;
+}
+
+function msgErrorStyle() {
   $msgsuccess.setAttribute("style", "display: none");
   $msgsuccess.innerHTML = "";
   $msgerror.setAttribute("style", "display: block");
-  $msgerror.innerHTML = `<strong>${$msg}</strong>`;
 }
 
 function resetFilds() {
